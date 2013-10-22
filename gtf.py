303605eefe7aef85d072174ca728de29b85c17b4
Jeff Hussmann <jeff.hussmann@gmail.com>
2013-10-22 13:27:07 -0500

    cleaned up gtf.py

--- gtf.py
+++ gtf.py
@@ -33,15 +33,6 @@ def get_all_genes(gtf_fn):
     all_genes = [parse_gtf_line(line) for line in open(gtf_fn)]
     return all_genes
 
-def get_contaminant_genes(gtf_fn):
-    def is_contaminant(gene):
-        return gene.source == 'rRNA' or gene.source == 'tRNA'
-    
-    all_genes = get_all_genes(gtf_fn)
-    contaminant_genes = [gene for gene in all_genes if is_contaminant(gene)]
-
-    return contaminant_genes
-
 def get_tRNA_genes(gtf_fn):
     all_genes = get_all_genes(gtf_fn)
     tRNA_genes = [gene for gene in all_genes if gene.source == 'tRNA']
@@ -54,67 +45,44 @@ def get_rRNA_genes(gtf_fn):
 
 def get_all_CDSs(gtf_fn):
     def is_CDS(gene):
-        return gene.source == 'protein_coding' and gene.feature == 'CDS'# and gene.end - gene.start > 1000
+        return gene.source == 'protein_coding' and gene.feature == 'CDS'
     
     all_genes = get_all_genes(gtf_fn)
     CDSs = [gene for gene in all_genes if is_CDS(gene)]
 
     return CDSs
 
-def get_simple_CDSs(gtf_fn):
-    ''' Returns all single exon CDSs that do not overlap any other CDS. '''
-    CDSs = get_all_CDSs(gtf_fn)
-    nonoverlapping = get_nonoverlapping_50(CDSs)
-    single_exons = get_single_exons(CDSs)
-    simple_CDSs = set(nonoverlapping) & set(single_exons)
-    simple_CDSs = sort_genes(list(simple_CDSs))
-    return simple_CDSs
-
-def get_nonoverlapping(genes):
-    ''' Returns all elements of genes that do not overlap any other element of
-        genes. Requires genes to be sorted by start.
-    '''
-    def overlaps(left, right):
-        return (right.seqname, right.start) <= (left.seqname, left.end)
-
-    overlapping = set()
-    nonoverlapping = []
-    for i, gene in enumerate(genes):
-        j = i + 1
-        while j < len(genes) and overlaps(gene, genes[j]):
-            overlapping.add(gene)
-            overlapping.add(genes[j])
-            j += 1
-        if gene not in overlapping:
-            nonoverlapping.append(gene)
-
-    return nonoverlapping
-
-def get_nonoverlapping_50(genes):
-    ''' Returns all elements of genes that do not overlap any other element of
-        genes. Requires genes to be sorted by start.
-    '''
-    def overlaps(left, right):
-        return (right.seqname, right.start - 50) <= (left.seqname, left.end + 50)
-
-    overlapping = set()
-    nonoverlapping = []
-    for i, gene in enumerate(genes):
-        j = i + 1
-        while j < len(genes) and overlaps(gene, genes[j]):
-            overlapping.add(gene)
-            overlapping.add(genes[j])
-            j += 1
-        if gene not in overlapping:
-            nonoverlapping.append(gene)
-
-    return nonoverlapping
-
 def sort_genes(genes):
     def key(gene):
         return gene.seqname, gene.start, gene.feature
 
     return sorted(genes, key=key)
+
+def get_nonoverlapping(genes, edge_buffer=0):
+    ''' Returns all elements of genes that do not overlap any other element of
+        genes..
+    '''
+    def overlaps(l, r):
+        ''' r is guaranteed to start after l because it comes after l
+            in a sorted list of genes, so r and l overlap iff r
+            hasn't ended by the time l starts.
+        '''
+        return (r.seqname, r.start - edge_buffer) <= (l.seqname, l.end + edge_buffer)
+
+    overlapping = set()
+    nonoverlapping = []
+
+    genes = sort_genes(genes)
+    for i, gene in enumerate(genes):
+        j = i + 1
+        while j < len(genes) and overlaps(gene, genes[j]):
+            overlapping.add(gene)
+            overlapping.add(genes[j])
+            j += 1
+        if gene not in overlapping:
+            nonoverlapping.append(gene)
+
+    return nonoverlapping
 
 def get_single_exons(CDSs):
     ''' Returns all CDSs that only have a single exon. '''
@@ -126,3 +94,12 @@ def get_single_exons(CDSs):
     single_exons = [exons[0] for protein, exons in proteins.items() if len(exons) == 1]
     single_exons = sort_genes(single_exons)
     return single_exons
+
+def get_simple_CDSs(gtf_fn):
+    ''' Returns all single exon CDSs that do not overlap any other CDS. '''
+    CDSs = get_all_CDSs(gtf_fn)
+    nonoverlapping = get_nonoverlapping(CDSs, edge_buffer=50)
+    single_exons = get_single_exons(CDSs)
+    simple_CDSs = set(nonoverlapping) & set(single_exons)
+    simple_CDSs = sort_genes(list(simple_CDSs))
+    return simple_CDSs
