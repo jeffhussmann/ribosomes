d8f8b9b8bf782ae7f8cc835efa543c936cfcb8d8
Jeff Hussmann <jeff.hussmann@gmail.com>
2013-10-25 11:57:02 -0500

    changed how much of edge_overlap is tracked

--- ribosomes.py
+++ ribosomes.py
@@ -107,26 +107,33 @@ def get_aggregate_positions(clean_bam_fn,
                             max_gene_length,
                             max_read_length,
                            ):
-    edge_overlap = max_read_length
+    edge_overlap = 50
+    # To some extent, this is linked to the definition of simple_CDS, which
+    # excludes genes that have another gene within 50 bp.
 
+    # position_counts will hold, for each gene, for each position from
+    # -edge_overlap to gene_length + edge_overlap
     position_counts = []
     gene_names = []
     for gene in simple_CDSs:
-        gene_names.append(gtf.parse_attribute(gene.attribute)['protein_id'])
-        shape = (3, abs(gene.end - gene.start) + 2 * edge_overlap + 1)
+        gene_name = gtf.parse_attribute(gene.attribute)['protein_id']
+        gene_length = abs(gene.end - gene.start) + 1  
+        gene_names.append((gene_name, gene_length))
+        shape = (3, 3 * edge_overlap + gene_length)
         position_counts.append(np.zeros(shape, int))
     expression_counts = np.zeros((len(simple_CDSs), 2), int)
 
-    # Dimension 2 goes from -edge_overlap to max_gene_length for from_starts
-    # and -max_gene_length to edge_overlap for from_ends.
-    shape = (max_read_length + 1, edge_overlap + max_gene_length + 1)
+    # For from_starts, dimension 2 should be interpretted as going from 
+    # -2 * edge_overlap to max_gene_length + edge_overlap.
+    # For from_ends, dimension 2 should be interpretted as going from 
+    # -(2 * edge_overlap + max_gene_length) to edge_overlap.
+    shape = (max_read_length + 1, 3 * edge_overlap + max_gene_length)
     from_starts = np.zeros(shape, int)
     from_ends = np.zeros(shape, int)
 
     fraction_shape = (max_read_length + 1, fraction_resolution)
     fractions = np.zeros(fraction_shape, int)
 
-    read_counts = defaultdict(lambda : defaultdict(int))
     nonunique_mappings = 0
 
     bamfile = pysam.Samfile(clean_bam_fn, 'rb')
@@ -155,15 +162,20 @@ def get_aggregate_positions(clean_bam_fn,
                         from_end = read.pos - (CDS.end + 1)
                     elif CDS.strand == '-':
                         from_start = CDS.end - (read.aend - 1)
-                            # CDS.start is the first base after the stop codon
+                        # CDS.start is the first base after the stop codon
                         from_end = (CDS.start - 1) - (read.aend - 1)
                 
+                start_index = 2 * edge_overlap + from_start 
+                from_starts[read.qlen, start_index] += 1
                 if 28 <= read.qlen <= 30:
-                    position_counts[g][read.qlen - 28, edge_overlap + from_start] += 1
-                    read_counts[CDS][strand] += 1
-                
-                from_starts[read.qlen, edge_overlap + from_start] += 1
-                from_ends[read.qlen, max_gene_length + from_end] += 1
+                    position_counts[g][read.qlen - 28, start_index] += 1
+                # For from_ends, index 2 * edge_overlap + max_gene_length
+                # corresponds to a read that starts right at the stop codon.
+                end_index = 2 * edge_overlap + max_gene_length + from_end
+                if 0 <= end_index < shape[1]:
+                    from_ends[read.qlen, end_index] += 1
+                else:
+                    print "bad from_end index"
                 
                 #if from_start >= 0:
                 #    if from_start == max_from_start:
@@ -173,49 +185,103 @@ def get_aggregate_positions(clean_bam_fn,
                 #    if fraction < fraction_resolution:
                 #        fractions[read.qlen, fraction] += 1
 
-    #with open(summary_fn, 'w') as summary_fh:
-    #    for CDS, counts in read_counts.items():
-    #        name = gtf.parse_attribute(CDS.attribute)['gene_name']
-    #        right_strand = CDS.strand
-    #        wrong_strand = '-' if CDS.strand == '+' else '+'
-    #        length = CDS.end - CDS.start + 1
-    #        keys = [right_strand, wrong_strand,
-    #                (28, 0), (28, 1), (28, 2),
-    #                (29, 0), (29, 1), (29, 2),
-    #                (30, 0), (30, 1), (30, 2),
-    #               ]
-    #        info = '\t'.join(str(counts[key]) for key in keys)
-    #        summary_fh.write('{0}\t{1}\t{2}\n'.format(name, length, info))
-
     return gene_names, position_counts, expression_counts, from_starts, from_ends
 
-def plot_RPF(from_starts_fns, from_ends_fns, names, read_lengths):
+def frameshifts(rpf_positions, edge_overlap):
+    rpf_positions = dict(zip(*rpf_positions))
+    gene_name = 'YLR233C'
+    gene_length = 2097
+    positions = rpf_positions[gene_name, gene_length][0]
+    start_at_codon = -4
+    codon_starts = np.arange(edge_overlap + (start_at_codon * 3),
+                             edge_overlap + gene_length,
+                             3,
+                            )
+    print codon_starts
+    print len(positions)
+    codon_numbers = [start_at_codon + i for i in range(len(codon_starts))]
+    frames = np.zeros((3, len(codon_starts)), int)
+    for c, codon_start in enumerate(codon_starts):
+        for frame in range(3):
+            frames[frame, c] = positions[codon_start + frame]
+
+    frames_so_far = frames.cumsum(axis=1)
+    fraction_frames_so_far = np.true_divide(frames_so_far, frames_so_far.sum(axis=0))
+
+    frames_remaining = np.fliplr(np.fliplr(frames).cumsum(axis=1))
+    fraction_frames_remaining = np.true_divide(frames_remaining, frames_remaining.sum(axis=0))
+    
+    print frames
+    return codon_numbers, fraction_frames_so_far, fraction_frames_remaining
+
+def plot_RPF(from_starts_fns, from_ends_fns, names):
+    edge_overlap = 50
     from_starts_list = [np.loadtxt(fn) for fn in from_starts_fns]
     from_ends_list = [np.loadtxt(fn) for fn in from_ends_fns]
 
     fig, (start_ax, end_ax) = plt.subplots(2, 1, figsize=(12, 16))
 
-    for from_starts, from_ends, name, read_length in zip(from_starts_list, from_ends_list, names, read_lengths):
-        starts_xs = np.arange(-read_length, 10000 + 1)
-        ends_xs = np.arange(-10000, read_length + 1)
-        for length in [22, 23, 24, 25, 28]:
+    experiments = zip(from_starts_list, from_ends_list, names)
+    for from_starts, from_ends, name in experiments:
+        starts_xs = np.arange(-2 * edge_overlap, 1000)
+        ends_xs = np.arange(-1000, edge_overlap)
+        for length in [28, 29, 30]:
             starts = np.true_divide(from_starts[length], from_starts[length].sum())
             ends = np.true_divide(from_ends[length], from_starts[length].sum())
-            start_ax.plot(starts_xs, starts, '.-', label=name + '_{0}'.format(length))
-            end_ax.plot(ends_xs, ends, '.-', label=name + '_{0}'.format(length))
+            start_ax.plot(starts_xs, starts[:len(starts_xs)],
+                          '.-',
+                          label=name + '_{0}'.format(length),
+                         )
+            end_ax.plot(ends_xs, ends[-len(ends_xs):],
+                        '.-',
+                        label=name + '_{0}'.format(length),
+                       )
 
     start_ax.set_xlim(-20, 20)
     start_ax.set_xlabel('Position of read relative to start of CDS')
-    start_ax.set_ylabel('Fraction of uniquely mapped reads')
-    #start_ax.set_ylim(ymax=0.008)
+    start_ax.set_ylabel('Fraction of uniquely mapped reads of specific length')
+    
     end_ax.set_xlim(-35, 5)
     end_ax.set_xlabel('Position of read relative to stop codon')
-    end_ax.set_ylabel('Fraction of uniquely mapped reads')
+    end_ax.set_ylabel('Fraction of uniquely mapped readsof specific length')
 
     leg = start_ax.legend(loc='upper right', fancybox=True)
     leg.get_frame().set_alpha(0.5)
     leg = end_ax.legend(loc='upper right', fancybox=True)
     leg.get_frame().set_alpha(0.5)
+
+def plot_density(from_starts_fns, from_ends_fns, names, read_lengths):
+    from_starts_list = [np.loadtxt(fn) for fn in from_starts_fns]
+    from_ends_list = [np.loadtxt(fn) for fn in from_ends_fns]
+
+    fig, (start_ax, end_ax) = plt.subplots(2, 1, figsize=(12, 16))
+
+    experiments = zip(from_starts_list, from_ends_list, names, read_lengths)
+    for from_starts, from_ends, name, read_length in experiments:
+        start_xs = np.arange(501)
+        end_xs = np.arange(-500, 1)
+        start_counts = from_starts[28][read_length + 1 - 12::3][:501]
+        print len(start_xs), len(start_counts)
+        end_counts = from_ends[28][-read_length - 15::-3][:501][::-1]
+        start_ax.plot(start_xs, start_counts,
+                      '.-',
+                     )
+        end_ax.plot(end_xs, end_counts,
+                    '.-',
+                   )
+
+    #start_ax.set_xlim(-20, 20)
+    #start_ax.set_xlabel('Position of read relative to start of CDS')
+    #start_ax.set_ylabel('Fraction of uniquely mapped reads of specific length')
+    #
+    #end_ax.set_xlim(-35, 5)
+    #end_ax.set_xlabel('Position of read relative to stop codon')
+    #end_ax.set_ylabel('Fraction of uniquely mapped readsof specific length')
+
+    #leg = start_ax.legend(loc='upper right', fancybox=True)
+    #leg.get_frame().set_alpha(0.5)
+    #leg = end_ax.legend(loc='upper right', fancybox=True)
+    #leg.get_frame().set_alpha(0.5)
 
 def plot_mRNA(from_starts_fns, from_ends_fns, fractions_fns, names, read_lengths, fragment_lengths):
     from_starts_list = [np.loadtxt(fn) for fn in from_starts_fns]
