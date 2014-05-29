3602f7981d11ad44b14dc140fa359fc62d22a573
Jeff Hussmann <jeff.hussmann@gmail.com>
2014-05-28 19:33:43 -0500

    factored out TIF_seq_structure

--- TIF_seq_experiment.py
+++ TIF_seq_experiment.py
@@ -12,6 +12,7 @@ from Sequencing.Parallel import map_reduce, split_file
 from Sequencing.utilities import reverse_complement, counts_to_array
 from Circles import adapters
 from Circles.adapters import find_adapter_positions, simple_hamming_distance
+import TIF_seq_structure
 
 mp1  = 'AGCGCTT'
 mp5  = 'CACTGTT'
@@ -36,137 +37,18 @@ for key, (string, mismatches) in forwards.items():
     else:
         new_key = key
     reverses[new_key] = (utilities.reverse_complement(string), mismatches)
-        
+
+# For reference:
+#CTCC AGGCGGCCGCTAT CACTCTGAGCAATACC
+#GTGG AGGCGGCCGCTAT ATCACTCTGAGCAATACC
+
+#  GGTATTGCTCAGAGTG ATAGCGGCCGCCT GGAG
+#GGTATTGCTCAGAGTGAT ATAGCGGCCGCCT CCAC
+
+#  GGTATTGCTCAGAGTG   ATAGCGGCCGCCT GGAG
+#  GGTATTGCTCAGAGTG   ATATAGCGGCCGCCT CCAC
+
 orientations = ['R1_forward', 'R1_reverse', 'R2_forward', 'R2_reverse']
-
-def valid_match(possible_seq, expected_seq, allowed_mismatches):
-    return (len(possible_seq) == len(expected_seq) and
-            simple_hamming_distance(possible_seq, expected_seq) <= allowed_mismatches)
-
-def find_structure(seq, adapters):
-    ''' Strategy: look for common middle or its RC, then see which of A or B is
-        on right and left.
-    '''
-    possibilities = []
-
-    middle_seq, middle_mismatches = adapters['middle']
-    ps = find_adapter_positions(seq, middle_seq, len(middle_seq), middle_mismatches)
-    for p in ps:
-        bad_left = False
-        bad_right = False
-
-        left_A_seq, left_A_mismatches = adapters['left_A']
-        left_B_seq, left_B_mismatches = adapters['left_B']
-        possible_left_A = seq[p - len(left_A_seq):p]
-        possible_left_B = seq[p - len(left_B_seq):p]
-
-        if valid_match(possible_left_A, left_A_seq, left_A_mismatches):
-            left_control_id = 'A'
-            left_p = p - len(left_A_seq)
-        elif valid_match(possible_left_B, left_B_seq, left_B_mismatches):
-            left_control_id = 'B'
-            left_p = p - len(left_B_seq)
-        else:
-            bad_left = True
-            left_control_id = possible_left_A
-
-        right_A_seq, right_A_mismatches = adapters['right_A']
-        right_B_seq, right_B_mismatches = adapters['right_B']
-        after_middle = p + len(middle_seq)
-        possible_right_A = seq[after_middle:after_middle + len(right_A_seq)]
-        possible_right_B = seq[after_middle:after_middle + len(right_B_seq)]
-        
-        # right_p is the index of the last base in 
-        if valid_match(possible_right_A, right_A_seq, right_A_mismatches):
-            right_control_id = 'A'
-            right_p = after_middle + len(right_A_seq) - 1
-        elif valid_match(possible_right_B, right_B_seq, right_B_mismatches):
-            right_control_id = 'B'
-            right_p = after_middle + len(right_B_seq) - 1
-        else:
-            bad_right = True
-            right_control_id = possible_right_A
-
-        if not (bad_left or bad_right):
-            possibility = [(left_p, right_p),
-                           (left_control_id, right_control_id),
-                          ]
-            possibilities.append(possibility)
-
-    if len(possibilities) > 1:
-        #raise ValueError(seq, possibilities)
-        return None
-    elif len(possibilities) == 0:
-        return None
-    else:
-        return possibilities.pop()
-
-def find_boundary_sequences(R1, R2, counters):
-    possibilities = {'R1_forward': find_structure(R1.seq, forwards),
-                     'R1_reverse': find_structure(R1.seq, reverses),
-                     'R2_forward': find_structure(R2.seq, forwards),
-                     'R2_reverse': find_structure(R2.seq, reverses),
-                    }
-    #TODO: use instances of R1/R2 overlap to extend UTR sequences
-    # For now, only valid if possible in only one.
-
-    valids = [key for key, possibility in sorted(possibilities.items()) if possibility]
-    counters['valids'][tuple(valids)] += 1
-
-    if len(valids) == 1:
-        orientation = valids.pop()
-        (left_p, right_p), (left_control_id, right_control_id) = possibilities[orientation]
-        if 'R1' in orientation:
-            seq = R1.seq
-            qual = R1.qual
-        elif 'R2' in orientation:
-            seq = R2.seq
-            qual = R2.qual
-
-        counters['positions'][orientation][left_p] += 1
-
-        left_slice = slice(None, left_p)
-        left_seq = seq[left_slice]
-        left_qual = qual[left_slice]
-        right_slice = slice(right_p + 1, None)
-        right_seq = seq[right_slice]
-        right_qual = qual[right_slice]
-
-        if 'forward' in orientation:
-            three_seq, three_qual = left_seq, left_qual
-            five_seq, five_qual = right_seq, right_qual
-            control_id_1, control_id_2 = left_control_id, right_control_id
-        elif 'reverse' in orientation:
-            three_seq, three_qual = reverse_complement(right_seq), right_qual[::-1]
-            five_seq, five_qual = reverse_complement(left_seq), left_qual[::-1]
-            control_id_1, control_id_2 = right_control_id, left_control_id
-            if control_id_1 != 'A' and control_id_1 != 'B':
-                control_id_1 = reverse_complement(control_id_1)
-            if control_id_2 != 'A' and control_id_2 != 'B':
-                control_id_2 = reverse_complement(control_id_2)
-
-        control_ids_string = '{0}-{1}'.format(control_id_1, control_id_2)
-        counters['control_ids'][control_ids_string] += 1
-
-        remove_poly_A_slice = slice(None, trim.find_poly_A(three_seq))
-        three_seq_trimmed = three_seq[remove_poly_A_slice]
-        three_qual_trimmed = three_qual[remove_poly_A_slice]
-        polyA_length = len(three_seq) - len(three_seq_trimmed)
-        counters['polyA_lengths'][orientation][polyA_length] += 1
-        
-        if len(five_seq) > 12 and len(three_seq_trimmed) > 12:
-            # Remove trailing read number identifier to allow IGV 'View as pairs'
-            common_name, _ = R1.name.rsplit('.', 1)
-            name = '{0}_{1}'.format(common_name, control_ids_string)
-            five_record = fastq.make_record(name, five_seq, five_qual)
-            three_record = fastq.make_record(name, three_seq_trimmed, three_qual_trimmed)
-        else:
-            five_record = None
-            three_record = None
-
-        return orientation, possibilities[orientation], five_record, three_record
-    else:
-        return None
 
 def trim_read_pairs(read_pairs):
     num_to_trim = len(mp1)
@@ -192,12 +74,8 @@ class TIFSeqExperiment(map_reduce.MapReduceExperiment):
             ('R1_reverse_positions', 'array_1d', '{name}_R1_reverse_positions.txt'),
             ('R2_forward_positions', 'array_1d', '{name}_R2_forward_positions.txt'),
             ('R2_reverse_positions', 'array_1d', '{name}_R2_reverse_positions.txt'),
-            ('R1_forward_polyA_lengths', 'array_1d', '{name}_R1_forward_polyA_lengths.txt'),
-            ('R1_reverse_polyA_lengths', 'array_1d', '{name}_R1_reverse_polyA_lengths.txt'),
-            ('R2_forward_polyA_lengths', 'array_1d', '{name}_R2_forward_polyA_lengths.txt'),
-            ('R2_reverse_polyA_lengths', 'array_1d', '{name}_R2_reverse_polyA_lengths.txt'),
+            ('polyA_lengths', 'array_1d', '{name}_polyA_lengths.txt'),
             ('control_ids', 'counts', '{name}_control_ids.txt'),
-            ('valids', 'counts', '{name}_valids.txt'),
             ('mapped_sam', 'sam_unsorted', '{name}_mapped.sam'),
             ('mapped_bam', 'bam', '{name}_mapped.bam'),
             ('mapped_bam_sorted', 'bam', '{name}_mapped_sorted.bam'),
@@ -213,12 +91,8 @@ class TIFSeqExperiment(map_reduce.MapReduceExperiment):
              'R1_reverse_positions',
              'R2_forward_positions',
              'R2_reverse_positions',
-             'R1_forward_polyA_lengths',
-             'R1_reverse_polyA_lengths',
-             'R2_forward_polyA_lengths',
-             'R2_reverse_polyA_lengths',
+             'polyA_lengths',
              'control_ids',
-             'valids',
              'mapped_bam_sorted',
             ],
         ]
@@ -278,11 +152,12 @@ class TIFSeqExperiment(map_reduce.MapReduceExperiment):
         trimmed_read_pairs = trim_read_pairs(read_pairs)
 
         total_reads = 0
+        long_enough = 0
     
         counters = {'positions': {orientation: Counter() for orientation in orientations},
                     'control_ids': Counter(),
-                    'polyA_lengths': {orientation: Counter() for orientation in orientations},
-                    'valids': Counter(),
+                    'polyA_lengths': Counter(),
+                    'well_formed': 0,
                    }
 
         with open(self.file_names['five_prime_boundaries'], 'w') as fives_fh, \
@@ -290,24 +165,24 @@ class TIFSeqExperiment(map_reduce.MapReduceExperiment):
 
             for R1, R2 in trimmed_read_pairs:
                 total_reads += 1
-                boundary_sequences = find_boundary_sequences(R1, R2, counters)
-                if boundary_sequences:
-                    orientation, structure, five_record, three_record = boundary_sequences
-                    if five_record and three_record:
-                        fives_fh.write(five_record)
-                        threes_fh.write(three_record)
+                five_record, three_record = TIF_seq_structure.find_boundary_sequences(R1, R2, counters)
+                if five_record and three_record:
+                    long_enough += 1
+                    fives_fh.write(five_record)
+                    threes_fh.write(three_record)
 
-        for suffix in ['positions', 'polyA_lengths']:
-            for orientation in orientations:
-                key = '{0}_{1}'.format(orientation, suffix)
-                array = counts_to_array(counters[suffix][orientation])
-                self.write_file(key, array)
+        for orientation in orientations:
+            key = '{0}_{1}'.format(orientation, 'positions')
+            array = counts_to_array(counters['positions'][orientation])
+            self.write_file(key, array)
 
         self.write_file('control_ids', counters['control_ids'])
-        self.write_file('valids', counters['valids'])
+        self.write_file('polyA_lengths', counts_to_array(counters['polyA_lengths']))
 
         self.log.extend(
             [('Total read pairs', total_reads),
+             ('Well-formed read pairs', counters['well_formed']),
+             ('Long enough', long_enough),
             ],
         )
 
@@ -349,7 +224,7 @@ class TIFSeqExperiment(map_reduce.MapReduceExperiment):
         fig, ax = plt.subplots()
 
         max_length = 0
-        for orientation in sorted(orientations):
+        for orientation in orientations:
             key = '{0}_positions'.format(orientation)
             array = self.read_file(key)
             max_length = max(max_length, len(array))
@@ -362,10 +237,8 @@ class TIFSeqExperiment(map_reduce.MapReduceExperiment):
     def plot_polyA_lengths(self):
         fig, ax = plt.subplots()
 
-        for orientation in sorted(orientations):
-            key = '{0}_polyA_lengths'.format(orientation)
-            array = self.read_file(key)
-            ax.plot(array, '.-', label=orientation)
+        array = self.read_file('polyA_lengths')
+        ax.plot(array, '.-', label='polyA_lengths')
 
         ax.legend(loc='upper right', framealpha=0.5)
         fig.savefig(self.figure_file_names['polyA_lengths'])
---
+++ TIF_seq_structure.py
@@ -0,0 +1,76 @@
+from Circles.adapters import find_adapter_positions, simple_hamming_distance
+from Sequencing import utilities, fastq
+import find_polyA
+
+common_right_reverse = 'GGTATTGCTCAGAGTGATA'
+after_right = {'A': 'GCGGCCGCCT',
+               'B': 'TAGCGGCCGCCT',
+              }
+after_left = {'A': 'GGAG',
+              'B' :'CCAC',
+             }
+after_right_length = 4
+after_right_prefix = {key: seq[:after_right_length] for key, seq in after_right.items()}
+
+def find_boundary_sequences(R1, R2, counters):
+    R1_ps = find_adapter_positions(R1.seq, common_right_reverse, 16, 1)
+    R2_ps = find_adapter_positions(R2.seq, common_right_reverse, 16, 1)
+
+    if len(R1_ps) + len(R2_ps) != 1:
+        return None, None
+    elif len(R1_ps) == 1:
+        reverse_read = R1
+        forward_read = R2
+        polyA_read = 'R2_forward'
+        p = R1_ps.pop()
+        counters['positions']['R1_reverse'][p] += 1
+    elif len(R2_ps) == 1:
+        reverse_read = R2
+        forward_read = R1
+        polyA_read = 'R1_forward'
+        p = R2_ps.pop()
+        counters['positions']['R2_reverse'][p] += 1
+
+    right_id = 'N'
+    left_id = 'N'
+
+    five_slice = slice(None, p)
+    five_seq = utilities.reverse_complement(reverse_read.seq[five_slice])
+    five_qual = reverse_read.qual[five_slice][::-1]
+
+    after_p = p + len(common_right_reverse)
+    if after_p < len(reverse_read.seq) - after_right_length:
+        right_id_seq = reverse_read.seq[after_p:after_p + after_right_length]
+        for key, prefix in after_right_prefix.items():
+            if right_id_seq == prefix:
+                right_id = key
+
+    if right_id != 'N':
+        after_p += len(after_right[right_id])
+        if after_p < len(reverse_read.seq) - 4:
+            left_id_seq = reverse_read.seq[after_p: after_p + 4]
+            for key, sequence in after_left.items():
+                if left_id_seq == sequence:
+                    left_id = key
+
+    polyA_start, poly_A_length = find_polyA.find_polyA(forward_read.seq, 15)
+    three_slice = slice(None, polyA_start)
+    three_seq = forward_read.seq[three_slice]
+    three_qual = forward_read.qual[three_slice]
+
+    # Remove trailing read number identifier to allow IGV 'View as pairs'
+    common_name, _ = R1.name.rsplit('.', 1)
+    control_ids_string = '{0}-{1}'.format(left_id, right_id)
+    name = '{0}_{1}'.format(common_name, control_ids_string)
+    five_record = fastq.make_record(name, five_seq, five_qual)
+    three_record = fastq.make_record(name, three_seq, three_qual)
+
+    counters['positions'][polyA_read][polyA_start] += 1
+    counters['polyA_lengths'][poly_A_length] += 1
+    counters['control_ids'][control_ids_string] += 1
+    counters['well_formed'] += 1
+
+    if p > 12 and polyA_start > 12: 
+        return five_record, three_record
+    else:
+        return None, None
