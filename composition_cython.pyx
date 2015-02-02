63712bdba35a0f81fce33f347a819cb11a1b1d4d
Jeff Hussmann <jeff.hussmann@gmail.com>
2015-02-01 22:44:00 -0600

    cleared numpy syntax from cython

--- composition_cython.pyx
+++ composition_cython.pyx
@@ -1,20 +1,20 @@
 import numpy as np
 from Sequencing import fastq
-cimport numpy as np
 cimport cython
-
-DTYPEINT = np.int
-ctypedef np.int_t DTYPEINT_t
 
 def length_stratified_composition(seq_info_pairs, int max_seq_length):
     ''' Unnecessary code duplication. '''
     cdef int i, b, seq_length
     cdef char* seq_typed
-    cdef char* bases = fastq.base_order[:5]
+    relevant_bases = fastq.base_order[:5]
+    cdef char* bases = relevant_bases
 
     shape = (max_seq_length + 1, max_seq_length + 1, 256)
-    cdef np.ndarray[DTYPEINT_t, ndim=3] all_array = np.zeros(shape, dtype=DTYPEINT)
-    cdef np.ndarray[DTYPEINT_t, ndim=3] perfect_array = np.zeros(shape, dtype=DTYPEINT)
+    all_array = np.zeros(shape, int)
+    perfect_array = np.zeros(shape, int)
+
+    cdef long[:, :, ::1] all_array_view = all_array
+    cdef long[:, :, ::1] perfect_array_view = perfect_array
     
     for seq, perfect_and_unique in seq_info_pairs:
         seq_length = len(seq)
@@ -22,9 +22,9 @@ def length_stratified_composition(seq_info_pairs, int max_seq_length):
         for i in range(seq_length):
             # Automatic type conversion means ord() is unneccesary
             b = seq_typed[i]
-            all_array[seq_length, i, b] += 1
+            all_array_view[seq_length, i, b] += 1
             if perfect_and_unique:
-                perfect_array[seq_length, i, b] += 1
+                perfect_array_view[seq_length, i, b] += 1
         
     # This pulls out only the columns corresponding to possible base
     # identities. 
