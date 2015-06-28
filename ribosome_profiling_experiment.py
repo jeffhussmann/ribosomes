22c8fb356e8ab69adac94ccd14634f452e7ec5db
Jeff Hussmann <jeff.hussmann@gmail.com>
2015-06-28 15:59:06 -0500

    record different edge excludsions at the same time

--- ribosome_profiling_experiment.py
+++ ribosome_profiling_experiment.py
@@ -794,58 +794,49 @@ class RibosomeProfilingExperiment(rna_experiment.RNAExperiment):
         #                                keys_to_plot=[28, 29, 30, 31, 32],
         #                               )
 
-    def compute_stratified_mean_enrichments(self, min_means=[0.1, 0]):
-        num_before = 90
-        num_after = 90
+    def compute_stratified_mean_enrichments(self,
+                                            min_means=[0.1],
+                                            num_around=100,
+                                            exclude_from_edges=[(90, 90),
+                                                                (200, 200),
+                                                                (200, 0),
+                                                                (0, 0),
+                                                               ],
+                                            do_anisomycin=False,
+                                           ):
 
-        def find_breakpoints(sorted_names, means, min_means):
-            breakpoints = {}
-
-            zipped = zip(sorted_names, means)
-            for min_mean in min_means:
-                name = [name for name, mean in zipped if mean > min_mean][-1]
-                breakpoints[name] = '{0:0.2f}'.format(min_mean)
-
-            return breakpoints
+        specific_keys = {'relaxed', 'identities'}
+        if do_anisomycin:
+            specific_keys.add('anisomycin')
 
         codon_counts = self.read_file('buffered_codon_counts',
-                                      specific_keys={'relaxed',
-                                                     'identities',
-                                                     'anisomycin',
-                                                    },
+                                      specific_keys=specific_keys,
                                      )
 
-        sorted_names, means = pausing.order_by_mean_density(codon_counts,
-                                                            count_type='relaxed',
-                                                            num_before=num_before,
-                                                            num_after=num_after,
-                                                           )
-        breakpoints = find_breakpoints(sorted_names, means, min_means)
-
         enrichments = pausing.fast_stratified_mean_enrichments(codon_counts,
-                                                               sorted_names,
-                                                               breakpoints,
-                                                               num_before,
-                                                               num_after,
+                                                               exclude_from_edges,
+                                                               min_means,
+                                                               num_around,
                                                                count_type='relaxed',
                                                               )
         self.write_file('stratified_mean_enrichments', enrichments)
 
-        sorted_names, means = pausing.order_by_mean_density(codon_counts,
-                                                            count_type='anisomycin',
-                                                            num_before=num_before,
-                                                            num_after=num_after,
-                                                           )
-        breakpoints = find_breakpoints(sorted_names, means, min_means)
+        if do_anisomycin:
+            sorted_names, means = pausing.order_by_mean_density(codon_counts,
+                                                                count_type='anisomycin',
+                                                                num_before=num_before,
+                                                                num_after=num_after,
+                                                               )
+            breakpoints = find_breakpoints(sorted_names, means, min_means)
 
-        enrichments = pausing.fast_stratified_mean_enrichments(codon_counts,
-                                                               sorted_names,
-                                                               breakpoints,
-                                                               num_before,
-                                                               num_after,
-                                                               count_type='anisomycin',
-                                                              )
-        self.write_file('stratified_mean_enrichments_anisomycin', enrichments)
+            enrichments = pausing.fast_stratified_mean_enrichments(codon_counts,
+                                                                   sorted_names,
+                                                                   breakpoints,
+                                                                   num_before,
+                                                                   num_after,
+                                                                   count_type='anisomycin',
+                                                                  )
+            self.write_file('stratified_mean_enrichments_anisomycin', enrichments)
 
     def plot_mismatches(self):
         type_counts = self.read_file('mismatches')
